d56b8a7186378d1be5046adce5025445acf40a6e
AuxiliumCDNG <soenke.klock@outlook.de>
2022-03-23 18:46:01 +0100

    weight script get pins from db

--- data_providers/weight.py
+++ data_providers/weight.py
@@ -1,11 +1,17 @@
-from lib.hx711 import HX711
-from database import Config
+import config
+import database
+
+if not config.Flask.debug:
+    from lib.hx711 import HX711
 
 def get():
-    hx = HX711(5, 6)
+    hx = HX711(
+        database.Config.query.filter_by(key="scale_dout").first().value,
+        database.Config.query.filter_by(key="scale_clk").first().value
+    )
 
-    hx.set_offset(Config.query.filter_by(key="scale_offset").first().value)
-    hx.set_scale(Config.query.filter_by(key="scale_ratio").first().value)
+    hx.set_offset(database.Config.query.filter_by(key="scale_offset").first().value)
+    hx.set_scale(database.Config.query.filter_by(key="scale_ratio").first().value)
 
     weight = hx.get_grams()
 
