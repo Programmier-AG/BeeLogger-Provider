1642787e4bc96916392dc51edb4c0e541cf3c078
AuxiliumCDNG <soenke.klock@outlook.de>
2022-03-24 23:02:59 +0100

    added logs table and fixed autoincrement bug

--- database.py
+++ database.py
@@ -10,8 +10,15 @@ class Config(client.Model):
 
 class DataBackup(client.Model):
     __tablename__ = "data_backup"
-    number = client.Column(client.BIGINT, nullable=False, primary_key=True, autoincrement=True)
+    number = client.Column(client.Integer, primary_key=True, autoincrement=True)
     temperature = client.Column(client.FLOAT, default=None)
     weight = client.Column(client.FLOAT, default=None)
     humidity = client.Column(client.FLOAT, default=None)
     measured = client.Column(client.DateTime, default=None)
+
+class Logs(client.Model):
+    id = client.Column(client.Integer, nullable=False, primary_key=True, autoincrement=True)
+    time = client.Column(client.DateTime)
+    source = client.Column(client.String(255))
+    message = client.Column(client.TEXT)
+    code = client.Column(client.Integer)
