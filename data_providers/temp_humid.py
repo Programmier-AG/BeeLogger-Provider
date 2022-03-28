c396af5b330bca6b9f3bcefd3e855d99242e17d2
AuxiliumCDNG <soenke.klock@outlook.de>
2022-03-28 16:11:37 +0200

    switched to more reliable dht lib

--- data_providers/temp_humid.py
+++ data_providers/temp_humid.py
@@ -2,25 +2,27 @@ import config
 import database
 
 if not config.Flask.debug:
-    import adafruit_dht
+    import Adafruit_DHT
 
 def get():
     model = database.Config.query.filter_by(key="dht_model").first().value
     pin = int(database.Config.query.filter_by(key="dht_dat").first().value)
 
     if model == "11":
-        dht = adafruit_dht.DHT11(
-            pin
-        )
+        sensor = Adafruit_DHT.DHT11
     elif model == "22":
-        dht = adafruit_dht.DHT22(
-            pin
-        )
+        sensor = Adafruit_DHT.DHT22
     else:
         print("No dht model was set! Skipping.")
-        return
+        return 0, 0
 
-    temp = dht.temperature
-    humid = dht.humidity
+    humid = temp = None
+    i = 0
+    while humid is None or temp is None:
+        if i > 5:
+            print("GETTING TEMP/HUMID FAILED AFTER 5 TRIES!!")
+            return 0, 0
+        i += 1
+        humid, temp = Adafruit_DHT.read_retry(sensor, pin)
 
     return temp, humid
